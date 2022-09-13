aa6e323a2c4c28037aaf56aebb21a86c8730753e
jayashankar.boya <jayashankar.boya@byjus.com>
2022-09-13 23:17:48 +0530

    Remove Additional list from Learnings.kt

--- app/src/main/java/com/example/myapplication/model/Learnings.kt
+++ app/src/main/java/com/example/myapplication/model/Learnings.kt
@@ -6,14 +6,6 @@ import com.example.myapplication.model.ItemType.EXPANDABLE_LIST_USING_COMPOSABLE
 object Learnings {
     val learningsList = listOf(
         LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = DATASTORE, title = "Data Store"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable"),
-        LearningDetails(id = EXPANDABLE_LIST_USING_COMPOSABLE, title = "Expandable list using composable")
+        LearningDetails(id = DATASTORE, title = "Data Store")
     )
 }
